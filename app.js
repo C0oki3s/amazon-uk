5ffd26ee8ac0cf93ede0a0411fbc15052dfd6a87
k_rohith <73307963+C0oki3s@users.noreply.github.com>
2024-05-25 01:33:07 +0000

    done

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const variables = {
         lat: 51.924491609412,
         lng: -0.487100640539,
         unit: "mi",
-        distance: 30,
+        distance: 200,
       },
       equalFilters: [],
       consolidateSchedule: true,
